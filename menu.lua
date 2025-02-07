6150fc34afa8d232e2295b224e7066dea101acba
ahmvdev <ahmadsaeed3290@gmail.com>
2025-02-07 15:57:39 +0500

    learned how to get display width of a terminal

--- menu.lua
+++ menu.lua
@@ -1,77 +1,22 @@
 local sys = require("system")
 
-local UI = require("ui")
-
-function displayMenu()
-    print("=============")
-    print("1. Check Time")
-    print("2. Get Mono Time")
-    print("3. Give Feedback")
-    print("4. Progress Bar Demo")
-    print("5. Logs ")
-    print("6. Exit")
-    print("=============")
+function menu()
+    print("1. Get Terminal Size")
+    print("2. Exit")
 end
 
-function sleep()
-    local input = io.read()
-    local gotInput = sys.readkey(input)
-    print(gotInput)
-end
-
-function getTime()
-    local time = sys.gettime()
-    local date = os.date("Current Time: %Y-%m-%d %H:%M:%S", time)
-    print(date)
-end
-
-
-function progressBar()
-    local total = 100
-    for i=1, total do
-        UI.progressBar(i, total)
-        sys.sleep(0.01)
-    end
-    print()
-end
-
-
-function monoTime()
-    local response = sys.monotime()
-    print(response)
-end
-
-function UI.prompt(message)
-    print(message .. " (yes/no):")
-    local response = io.read()
-    if response == "yes" then
-        return true
-    else return false end
-end
-
-function uiPrompt()
-    local response = UI.prompt("Do you like lua?")
-        if response == true then 
-            print("Thats great!")
-        else 
-            print("So sad to hear :(")
-        end
+function termsize()
+   k,v = sys.termsize()
+    print("Height: ".. k .. " " .. "Width: " .. v)
 end
 
 while true do
-    displayMenu()
-    io.write("Select an Option: ")
+    menu()
     local choice = tonumber(io.read())
-
     if choice == 1 then
-        getTime()
+        termsize()
     elseif choice == 2 then
-        monoTime()
-    elseif choice == 3 then
-        uiPrompt()
-    elseif choice == 4 then
-        progressBar()
-    elseif choice==6 then
+        os.exit()
         break
     end
 end
