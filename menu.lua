8c9e55ca814dbc19acdf48ca7f0a5979a5b9e6e1
ahmvdev <ahmadsaeed3290@gmail.com>
2025-03-11 02:31:12 +0500

    updates to menu.lua added copas task scheduling for time and terminal

--- menu.lua
+++ menu.lua
@@ -1,8 +1,11 @@
 local sys = require("system")
+local copas = require("copas")
+
 
 function menu()
     print("1. Get Terminal Size")
-    print("2. Exit")
+    print("2. Check time")
+    print("3. Exit")
 end
 
 function termsize()
@@ -10,13 +13,46 @@ function termsize()
     print("Height: ".. k .. " " .. "Width: " .. v)
 end
 
+function gettime()
+  copas.addthread( 
+  function()  
+  while run do 
+    local systime = sys.gettime()
+    io.write("\r" , math.floor(systime))
+    io.flush()
+    copas.pause(0.1)
+end
+end)
+end    
+
+copas.addthread( function()
 while true do
     menu()
-    local choice = tonumber(io.read())
+    local character = sys.readansi(math.huge, copas.pause)
+    local choice = tonumber(character)
     if choice == 1 then
         termsize()
     elseif choice == 2 then
-        os.exit()
+        run = true
+        gettime()
+    elseif choice == 3 then
+        run = false
         break
     end
+    copas.pause(0.1)
 end
+end)
+
+local of_attr = sys.tcgetattr(io.stdin)
+sys.setnonblock(io.stdin, true)
+sys.tcsetattr(io.stdin, sys.TCSANOW, {
+  lflag = of_attr.lflag - sys.L_ICANON - sys.L_ECHO, -- disable canonical mode and echo
+})
+
+copas.loop()
+
+local of_attr = sys.tcgetattr(io.stdin)
+sys.setnonblock(io.stdin, false)
+sys.tcsetattr(io.stdin, sys.TCSANOW, {
+  lflag = of_attr.lflag + sys.L_ICANON + sys.L_ECHO,
+})
